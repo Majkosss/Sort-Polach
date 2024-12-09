601366efc3a66c852893f80de54aa9ea2c846a43
Michal P <159123359+Majkosss@users.noreply.github.com>
2024-12-09 08:07:47 +0000

    dwdsdw

--- main.py
+++ main.py
@@ -1,45 +1,45 @@
 array = [42, 10, 78, 54, 23, 7, 85, 92, 31, 67]
 
 # Bubble sort
-def bubble_sort(arr):
-    n = len(arr)
+def bubble_sort(array):
+    n = len(array)
     for i in range(n):
         for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-    return arr
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
+    return array
 
 # Bogo sort
 import random
-def is_sorted(arr):
-    return all(arr[i] <= arr[i+1] for i in range(len(arr)-1))
+def is_sorted(array):
+    return all(array[i] <= array[i+1] for i in range(len(array)-1))
 
-def bogo_sort(arr):
-    while not is_sorted(arr):
-        random.shuffle(arr)
-    return arr
+def bogo_sort(array):
+    while not is_sorted(array):
+        random.shuffle(array)
+    return array
 
 # Selection sort
-def selection_sort(arr):
-    n = len(arr)
+def selection_sort(array):
+    n = len(array)
     for i in range(n):
         min_idx = i
         for j in range(i+1, n):
-            if arr[j] < arr[min_idx]:
+            if array[j] < array[min_idx]:
                 min_idx = j
-        arr[i], arr[min_idx] = arr[min_idx], arr[i]
-    return arr
+        array[i], array[min_idx] = array[min_idx], array[i]
+    return array
 
 # Insertion sort
-def insertion_sort(arr):
-    for i in range(1, len(arr)):
-        key = arr[i]
+def insertion_sort(array):
+    for i in range(1, len(array)):
+        key = array[i]
         j = i - 1
-        while j >= 0 and key < arr[j]:
-            arr[j + 1] = arr[j]
+        while j >= 0 and key < array[j]:
+            array[j + 1] = array[j]
             j -= 1
-        arr[j + 1] = key
-    return arr
+        array[j + 1] = key
+    return array
 
 print("Základní pole:", array)
 print("Bubble sort:", bubble_sort(array.copy()))
